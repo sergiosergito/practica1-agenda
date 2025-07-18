b83754de4f09a75aa90fa298c281c8ecc4d2c1cf
sergiosergito <serginho_6x@hotmail.com>
2025-07-18 01:24:18 -0400

    feat: Editar implementado

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -70,6 +70,66 @@
           </th>
           <th></th>
         </tr>
+        <tr>
+          <th scope="col">
+            <button
+              type="button"
+              class="btn btn-primary"
+              @click="confirmarEditar()"
+            >
+              Editar
+            </button>
+          </th>
+          <th scope="col">
+            <input
+              type="text"
+              v-model="editContactName"
+              class="form-control"
+              placeholder="Ingresar nombre"
+            />
+          </th>
+          <th scope="col">
+            <input
+              type="text"
+              v-model="editContactEmail"
+              class="form-control"
+              placeholder="Ingresar correo"
+            />
+          </th>
+          <th scope="col">
+            <input
+              type="text"
+              v-model="editContactAddress"
+              class="form-control"
+              placeholder="Ingresar Dirección"
+            />
+          </th>
+          <th scope="col">
+            <input
+              type="text"
+              v-model="editContactPhone"
+              class="form-control"
+              placeholder="Ingresar teléfono"
+            />
+          </th>
+          <th scope="col">
+            <input
+              type="text"
+              v-model="editContactCountry"
+              class="form-control"
+              placeholder="Ingresar País"
+            />
+          </th>
+          <th scope="col">
+            <input
+              type="text"
+              v-model="editContactCity"
+              class="form-control"
+              placeholder="Ingresar Ciudad"
+            />
+          </th>
+          <th></th>
+        </tr>
       </thead>
       <tbody>
         <tr v-for="(item, index) in obtenerLista" :key="item.id">
@@ -84,7 +144,7 @@
             <button
               type="button"
               class="btn btn-primary"
-              @click="abrirModal(index)"
+              @click="startEditing(item)"
             >
               Editar
             </button>
@@ -127,6 +187,12 @@ export default {
       newContactCountry: "",
       newContactCity: "",
       newContact: null,
+      editContactName: "",
+      editContactEmail: "",
+      editContactAddress: "",
+      editContactPhone: "",
+      editContactCountry: "",
+      editContactCity: "",
     };
   },
   mounted() {},
@@ -142,6 +208,21 @@ export default {
       if (this.contacts.length === 0) return 1;
       const maxId = Math.max(...this.contacts.map((c) => c.id));
       return maxId + 1;
+    },
+
+    borrarCampos() {
+      this.newContactName = "";
+      this.newContactEmail = "";
+      this.newContactAddress = "";
+      this.newContactPhone = "";
+      this.newContactCountry = "";
+      this.newContactCity = "";
+      this.editContactName = "";
+      this.editContactEmail = "";
+      this.editContactAddress = "";
+      this.editContactPhone = "";
+      this.editContactCountry = "";
+      this.editContactCity = "";
     },
 
     crearNuevo() {
@@ -165,14 +246,46 @@ export default {
         );
         console.log(this.newContact);
         this.contacts.push(this.newContact);
+        this.borrarCampos();
       } else {
         this.newContact = null;
-        this.newContactName = "";
-        this.newContactEmail = "";
-        this.newContactAddress = "";
-        this.newContactPhone = "";
-        this.newContactCountry = "";
-        this.newContactCity = "";
+        this.borrarCampos();
+      }
+    },
+    startEditing(contact) {
+      this.editingContactId = contact.id;
+      this.editContactName = contact.name;
+      this.editContactEmail = contact.email;
+      this.editContactAddress = contact.address;
+      this.editContactPhone = contact.phone;
+      this.editContactCountry = contact.country;
+      this.editContactCity = contact.city;
+    },
+    confirmarEditar() {
+      if (
+        this.editContactName.trim() &&
+        this.editContactEmail.trim() &&
+        this.editContactAddress.trim() &&
+        this.editContactPhone.trim() &&
+        this.editContactCity.trim() &&
+        this.editContactCountry.trim()
+      ) {
+        const contact = this.contacts.find(
+          (c) => c.id === this.editingContactId
+        );
+        if (contact) {
+          contact.name = this.editContactName;
+          contact.email = this.editContactEmail;
+          contact.address = this.editContactAddress;
+          contact.phone = this.editContactPhone;
+          contact.country = this.editContactCountry;
+          contact.city = this.editContactCity;
+          this.editingContactId = null;
+        }
+        console.log(contact);
+        this.borrarCampos();
+      } else {
+        this.borrarCampos();
       }
     },
   },

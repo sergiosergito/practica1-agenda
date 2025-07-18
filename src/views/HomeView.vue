8b0a30df9bd33e4696a9cc2da8fe9009427333d3
sergiosergito <serginho_6x@hotmail.com>
2025-07-17 23:28:55 -0400

    feat: Tabla creada

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,47 @@
 <template>
   <div class="Main">
     <HelloWorld msg="Agenda de contactos" />
-    <ul>
-      <li v-for="contact in contacts" :key="contact.id" class="item">
-        {{ contact.name }} - {{ contact.name }}
-      </li>
-    </ul>
+    <table class="table table-bordered border-primary">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Address</th>
+          <th scope="col">Phone</th>
+          <th scope="col">Country</th>
+          <th scope="col">City</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in obtenerLista" :key="item.id">
+          <th scope="row">{{ item.id }}</th>
+          <td>{{ item.name }}</td>
+          <td>{{ item.email }}</td>
+          <td>{{ item.address }}</td>
+          <td>{{ item.phone }}</td>
+          <td>{{ item.country }}</td>
+          <td>{{ item.city }}</td>
+          <td>
+            <button
+              type="button"
+              class="btn btn-primary"
+              @click="abrirModal(index)"
+            >
+              Editar
+            </button>
+            <button
+              type="button"
+              class="btn btn-danger"
+              @click="eliminar(index)"
+            >
+              eliminar
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -22,18 +58,42 @@ export default {
   data() {
     return {
       title: "Contacts",
-      contacts: [],
+      contacts: getContacts(),
       modalBootstrapInstance: null,
       modalMode: "crear",
       itemSeleccionado: null,
     };
   },
-  mounted() {
-    this.obtenerLista();
-  },
+  mounted() {},
   methods: {
+    abrirModal(index) {
+      //this.autoSeleccionado = null;
+      //this.indiceSeleccionado = index;
+      //this.modalMode = "editar";
+      //setTimeout(() => {
+      /*
+        if (this.modalBootstrapInstance) {
+          this.modalBootstrapInstance.show();
+          this.autoSeleccionado = { ...this.items[index] };
+        } else {
+          console.error("modalBootstrapInstance no está inicializado");
+        }*/
+      //});
+      console.log(`editando ${index}`);
+    },
+    eliminar(index) {
+      console.log(`eliminando ${index}`);
+      /*
+      if (confirm("¿Está seguro de eliminar este auto?")) {
+        this.items.splice(index, 1);
+      }
+      */
+    },
+  },
+  computed: {
     obtenerLista() {
-      this.contacts = getContacts();
+      let results = [...this.contacts];
+      return results;
     },
   },
 };
